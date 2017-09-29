864d29242d27ac0b5963a42e2e2793635af53b66
XUENN\blackie.tsai <blackie1019@gmail.com>
2017-09-29 19:16:53 +0800

    Init

--- app/Program.cs
+++ app/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Reactive;
+using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using System.Threading;
+using System.Reactive.PlatformServices;
+using System.Linq;
 
 namespace app {
     public class Program {
@@ -12,23 +15,29 @@ namespace app {
             
             Demo();
 
-            Console.WriteLine("Press any key to exit the program...");
-            Console.ReadKey();
+            Console.WriteLine("ByeBye");
         }
 
         static void Demo(){
-            var isExisted = false;
+            var isContinue = true;
             
             do{
                 Console.WriteLine("======Press demo key to represent...=====");
                 Console.WriteLine("(a) Demo MyObserver");
                 Console.WriteLine("(b) Demo MyObserver with Lambda");
-                Console.WriteLine("(c) Demo MyObservable");
+                Console.WriteLine("(c) Demo MyObserver with Lambda and NewThread");
+                Console.WriteLine("(d) Demo MyObservable");
+                Console.WriteLine("(e) Demo Lab-1");
+                Console.WriteLine("(f) Demo Lab-2");
                 Console.WriteLine("(q) Exit Menu");
 
                 var keyInfo = Console.ReadKey();
+                Console.WriteLine();
 
                 switch(keyInfo.Key){
+                    case ConsoleKey.Q:
+                        isContinue = false;
+                        break;
                     case ConsoleKey.A:
                         Demo_MyObserver();
                         break;
@@ -36,16 +45,39 @@ namespace app {
                         Demo_MyObserverLambda();
                         break;
                     case  ConsoleKey.C:
+                        Demo_MyObserverLambdaWithNewThread();
+                        break;
+                    case  ConsoleKey.D:
                         Demo_MyObservable();
+                        break;
+                    case  ConsoleKey.F:
+                        Demo_Lab_2();
                         break;
                     default:
                         Console.WriteLine("Unknow, please re-try!!");
                         break;
                 } 
-                        
-                isExisted = (keyInfo.Key == ConsoleKey.Q);
 
-            }while(isExisted); 
+            }while(isContinue); 
+        }
+
+        static void Demo_Lab_2(){
+            var result = 
+                from i in Observable.Range(1, 100) 
+                from j in Observable.Range(1, 100) 
+                from k in Observable.Range(1, 100) 
+                where k * k == i * i + j * j 
+                select new { a = i, b = j, c = k };   
+        
+            // A Subscriber with 
+            // A callback (Lambda) which prints value, 
+            // A callback for Exception 
+            // A callback for Completion  
+        
+            IDisposable subscription = result.Subscribe( 
+            x => Console.WriteLine("OnNext: {0}", x),  
+            ex => Console.WriteLine("OnError: {0}", ex.Message), 
+            () => Console.WriteLine("OnCompleted")); 
         }
 
         static void Demo_MyObservable () {
@@ -57,9 +89,10 @@ namespace app {
 
             Console.WriteLine("Press any key to dispose the subscription...");
             Console.ReadKey();
+            Console.WriteLine();
             subscription.Dispose();
         }
-
+        
         static void Demo_MyObserverLambda () {
             var observable = Observable.Range(5, 8);
             
@@ -71,7 +104,35 @@ namespace app {
 
             Console.WriteLine("Press any key to dispose the subscription...");
             Console.ReadKey();
+            Console.WriteLine();
             subscription.Dispose();
+        }
+
+
+        static void Demo_MyObserverLambdaWithNewThread () {
+            
+            //var query = from number in Enumerable.Range(5,8) select number;
+            
+            //var observable = query.ToObservable(NewThreadScheduler.Default);
+
+            // var subscription = observable.Subscribe(
+            //     (input) =>{Console.WriteLine($"Id:{Thread.CurrentThread.ManagedThreadId.ToString()}:{input}");},
+            //     (err) => {Console.WriteLine(err.Message);},
+            //     () => {Console.WriteLine("completed");}
+            // );
+
+            var source = Observable.Range(5, 8)
+                .SelectMany(i=>Observable.Start(()=>i, NewThreadScheduler.Default))
+                //.ObserveOn(NewThreadScheduler.Default)
+                .Subscribe(
+                    (input) =>{Console.WriteLine($"Id:{Thread.CurrentThread.ManagedThreadId.ToString()}:{input}");},
+                    (err) => {Console.WriteLine(err.Message);},
+                    () => {Console.WriteLine("completed");});
+
+            Console.WriteLine("Press any key to dispose the subscription...");
+            Console.ReadKey();
+            Console.WriteLine();
+            source.Dispose();
         }
 
         static void Demo_MyObserver () {
@@ -83,6 +144,7 @@ namespace app {
 
             Console.WriteLine("Press any key to dispose the subscription...");
             Console.ReadKey ();
+            Console.WriteLine();
             subscription.Dispose ();
         }
 
