4c69c278bc6085ae0b06adf9e4a3dcb0ffffabe1
XUENN\blackie.tsai <blackie1019@gmail.com>
2017-10-04 16:41:13 +0800

    新增 Lab_01

---
+++ Lab_01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace Lab_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IObservable<string> strings =
+                new[] { "Jordan", "Kobe", "James", "Durant" }.ToObservable(); 
+
+            IDisposable subscription =  
+                strings.Where(str => str.StartsWith("J"))  
+                    .Select(str => str.ToUpper()) 
+                    .Subscribe(Console.WriteLine); 
+
+            subscription.Dispose();
+        }
+    }
+}
